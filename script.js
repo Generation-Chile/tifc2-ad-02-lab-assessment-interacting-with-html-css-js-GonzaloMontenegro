5d65acdc5cd57f2c99026a37a336b36b6fde1df6
Gonzalo Montenegro <knxeee@gmail.com>
2025-04-24 12:41:57 -0400

    Trabajo finalizado

---
+++ script.js
@@ -0,0 +1,6 @@
+// Modificar el primero "!Hola mundo!" en Js por "Adios"
+document.getElementsByTagName("h1")[0].innerHTML = "Adios"
+document.getElementsByTagName("h5")[0].style.color = "orange"
+document.getElementById("boton").addEventListener("click", function () {
+  this.style.color("brown");
+});
